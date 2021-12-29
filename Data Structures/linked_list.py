0bd74b241771170d7ade3d5c2b3096addad973c9
GithubArnavSharma <77365987+GithubArnavSharma@users.noreply.github.com>
2021-12-28 18:56:17 -0800

    Update linked_list.py

--- Data Structures/linked_list.py
+++ Data Structures/linked_list.py
@@ -34,7 +34,38 @@ class LinkedList:
       
     return count
   
-
+  def add(self, data):
+    """
+    Prepends a new node with data 'data' to the head of the list
+    Time Complexity: O(1)
+    """
+    
+    new_node = Node(data)
+    new_node.next_node = self.head
+    self.head = new_node
+    
+  def __repr__(self):
+    """
+    Returns a string representing the list
+    Time Complexity: O(n)
+    """
+    
+    nodes = []
+    current = self.head
+    
+    while current is not None:
+      if current is self.head:
+        nodes.append("[Head: %s]" % current.data)
+      elif current.next_node is None:
+        nodes.append("[Tail: %s]" % current.data)
+      else:
+        nodes.append("[%s]" % current.data)
+      
+      current = current.next_node
+      
+    return " -> ".join(nodes)
+    
+  
 N1 = Node(10)
 N2 = Node(20)
 N1.next_node = N2
@@ -42,3 +73,10 @@ N1.next_node = N2
 l = LinkedList()
 l.head = Node(10)
 print(l.size())
+
+l = LinkedList()
+l.add(1)
+l.add(2)
+l.add(3)
+print(l.size())
+print(l)
