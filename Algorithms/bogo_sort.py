2b61cd7f5cf5914b8b843f18ee09a4b7d718a77b
GithubArnavSharma <77365987+GithubArnavSharma@users.noreply.github.com>
2021-12-30 19:21:14 -0800

    Update bogo_sort.py

--- Algorithms/bogo_sort.py
+++ Algorithms/bogo_sort.py
@@ -3,7 +3,7 @@ import random
 numbers = [5,3,8,9,2,4]
 
 def is_sorted(the_list):
-  for index in range(len(the_list)-1):
+  for index in range(0, len(the_list)-1):
     if the_list[index] > the_list[index + 1]:
       return False
   return True
